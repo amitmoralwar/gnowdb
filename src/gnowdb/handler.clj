b50820a8cc84d800922cf7eda9a2a405fd6e337f
Anant Sujatanagarjuna <anant.essen@gmail.com>
2018-10-26 18:57:04 +0530

    REST API will now wrap Vectors and Maps properly with json. Not tested completely but works.  Changes to be committed: 	modified:   src/gnowdb/handler.clj 	modified:   src/gnowdb/routes/gneoauto.clj

--- src/gnowdb/handler.clj
+++ src/gnowdb/handler.clj
@@ -6,7 +6,7 @@
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.nested-params :refer [wrap-nested-params]]
-            [ring.middleware.json :refer [wrap-json-params]]
+            [ring.middleware.json :refer [wrap-json-params wrap-json-response wrap-json-body]]
             [ring.util.response :refer [response]]
             [ring.middleware.json :refer [wrap-json-response]]
             [ring.middleware.session :refer [wrap-session]]
@@ -36,6 +36,7 @@
   (println "liberator-service is shutting down"))
 
 (defroutes app-routes
+  gneo-auto-routes
   (route/resources "/")
   (route/not-found "Not Found"))
 
@@ -43,21 +44,21 @@
 
 
 (def app
-  (-> (routes gneo-routes gneo-auto-routes workspaces-routes files-routes login-routes app-routes)  
-      
-      (friend/authenticate {:credential-fn (partial creds/bcrypt-credential-fn @users)
-                            :workflows [(workflows/interactive-form)]
-                            :allow-anon? true
-                            :login-uri "/login"
-                            :default-landing-uri "/api"})  
-      (wrap-keyword-params)
-      (wrap-params)      
-      (wrap-nested-params)
-      (wrap-json-params)
-      
-      (wrap-session)    
-      (handler/site)
+  (-> 
+   (handler/api app-routes)
+   ;; (routes gneo-routes gneo-auto-routes workspaces-routes files-routes login-routes app-routes)  
+   
+   ;; (friend/authenticate {:credential-fn (partial creds/bcrypt-credential-fn @users)
+   ;;                       :workflows [(workflows/interactive-form)]
+   ;;                       :allow-anon? true
+   ;;                       :login-uri "/login"
+   ;;                       :default-landing-uri "/api"})  
+   (wrap-params)
+   (wrap-keyword-params)
+   (wrap-nested-params)
+   (wrap-json-body)
+   (wrap-json-response)
+   (wrap-json-params)
+   (wrap-session)    
+   ;; (handler/site)
    ))
-
-
-
--- src/gnowdb/routes/gneoauto.clj
+++ src/gnowdb/routes/gneoauto.clj
@@ -7,4 +7,4 @@
            (POST "/:restapifunc" request
                  (let [restfunc (var-get (ns-resolve 'gnowdb.neo4j.gneo (symbol (:restapifunc (:params request)))))
                               json-params (into {} (for [[k v] (:json-params request)] [(keyword k) v]))] ;; Converting the first level of string keys to keywords for passing to gneo functions https://stackoverflow.com/a/9406251/6767262
-                          (apply restfunc (prepMapAsArg json-params))))))
+                          {:body (apply restfunc (prepMapAsArg json-params))}))))
